da86c526233d5f8f901820229c937ad9fcf54621
millio <millio@watcha.com>
2021-07-05 14:33:11 +0900

    테니스게임 3 리팩토링

--- kotlin/src/main/kotlin/TennisGame3.kt
+++ kotlin/src/main/kotlin/TennisGame3.kt
@@ -4,25 +4,31 @@ class TennisGame3(private val p1N: String, private val p2N: String) : TennisGame
     private var p1: Int = 0
 
     override fun getScore(): String {
-        val s: String
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            val p = arrayOf("Love", "Fifteen", "Thirty", "Forty")
-            s = p[p1]
-            return if (p1 == p2) "$s-All" else "$s-${p[p2]}"
+        if (p1 < 4 && p2 < 4 && p1 + p2 != 6) {
+            val p = TennisScore.values().map { it.scoreName }
+            val p1Score = p[p1]
+            val p2Score = p[p2]
+            val result = if (p1 == p2) DEUCE_UNDER_FORTY else p2Score
+            return "$p1Score$HYPHEN$result"
+        } else if (p1 == p2) {
+            return DEUCE_SCORE_NAME
         } else {
-            if (p1 == p2)
-                return "Deuce"
-            s = if (p1 > p2) p1N else p2N
-            return if ((p1 - p2) * (p1 - p2) == 1) "Advantage $s" else "Win for $s"
+            val name = if (p1 > p2) p1N else p2N
+            val score = if ((p1 - p2) * (p1 - p2) == 1) ADVANTAGE_SCORE else WON_SCORE
+            return "$score $name"
         }
     }
 
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            this.p1 += 1
-        else
-            this.p2 += 1
-
+        upPlayerScore(playerName)
     }
+
+    private fun upPlayerScore(winnerName: String) {
+        if (winnerName === "player1")
+            p1++
+        else
+            p2++
+    }
+
 
 }
